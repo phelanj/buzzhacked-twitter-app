48b3a40bab544f1689c2f2720d7deb62c312db2c
Jack Phelan <phelanj@users.noreply.github.com>
2016-09-26 13:25:46 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,19 +2,17 @@
 require_once('TwitterAPIExchange.php');
  
 /** Set access tokens here - see: https://dev.twitter.com/apps/ **/
-$settings = array(
-    'oauth_access_token' => "2818292279-3MFeQWmr8fnW7D1jxWM4O0iLEzC5ZCFcqkE1keH",
-    'oauth_access_token_secret' => "1RVo2b5VBDpvK0IDEeHfdMCCdW3PPCLiId85Sa4gPWwJz",
-    'consumer_key' => "TVZvWLVztfcwf3FteoGcoZV2j",
-    'consumer_secret' => "zpVqhzqePYnFhBCrNTYOhZfRwb8afeuvPKU8EEs5m4MZ0TGLii"
-);
-$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
-$requestMethod = "GET";
-$getfield = '?screen_name=realDonaldTrump&count=5';
-
-$twitter = new TwitterAPIExchange($settings);
-echo $twitter->setGetfield($getfield)
-             ->buildOauth($url, $requestMethod)
+$twitter =  new TwitterAPIExchange(array(
+    'oauth_access_token' => "780498354874650624-OoMQTkuwnddOXyW2XN1qdch7afES6r7",
+    'oauth_access_token_secret' => "yFgK0JWVlfrM2dqrze08lYso8AwWXSvooYgnzN98rcO1O",
+    'consumer_key' => "kMq5g5DVxDxAlH8Dq0Z2LVNDy",
+    'consumer_secret' => "K5IOhKvnM5xFB5auuEa1DLzuQ7I720kXSgHCTfxGpOM2cupKV1"
+));
+$url = 'https://api.twitter.com/1.1/statuses/update.json';
+$requestMethod = 'POST';
+$postfields = array('status' => 'Have you been #pwned?');
+echo $twitter->buildOauth($url, $requestMethod)
+             ->setPostfields($postfields)
              ->performRequest();
 
 ?>
