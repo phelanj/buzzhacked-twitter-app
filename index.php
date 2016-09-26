73890b3c9dccf7e2e07aa2d209ab91fa676296bf
phelanj <jackphelan461@gmail.com>
2016-09-26 12:12:01 -0700

    twitter api

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-echo "<h2>Simple Twitter API Test</h2>";
+require_once('TwitterAPIExchange.php');
+ 
+/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
+$settings = array(
+    'oauth_access_token' => "2818292279-3MFeQWmr8fnW7D1jxWM4O0iLEzC5ZCFcqkE1keH",
+    'oauth_access_token_secret' => "1RVo2b5VBDpvK0IDEeHfdMCCdW3PPCLiId85Sa4gPWwJz",
+    'consumer_key' => "TVZvWLVztfcwf3FteoGcoZV2j",
+    'consumer_secret' => "zpVqhzqePYnFhBCrNTYOhZfRwb8afeuvPKU8EEs5m4MZ0TGLii"
+);
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+$getfield = '?screen_name=realDonaldTrump&count=5';
+
+$twitter = new TwitterAPIExchange($settings);
+echo $twitter->setGetfield($getfield)
+             ->buildOauth($url, $requestMethod)
+             ->performRequest();
+
 ?>
